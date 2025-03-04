39a2cde3db0c238dd1e49577bdea6518e3379c26
AzizVerse <benmahmoud0499@gmail.com>
2025-03-04 15:45:26 -0500

    Implemented Error Handling for 405 Method Not Allowed

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -123,4 +123,8 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)  # DELETE is not allowed on list endpoint
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
